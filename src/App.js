9c829eb4964709c0127209c17c3c36c209f3d79c
alitemel89 <alitemel89.ce@gmail.com>
2021-06-05 20:14:45 +0300

    update geojson file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
-import { MapContainer, GeoJSON, TileLayer } from "react-leaflet";
-import agriculturalAreas from "./data/agriculture_turkey.json";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+import agriculturalAreas from "./data/agriculture_turkey.json";
+
 function App() {
-   
-   
-    const totalArea = agriculturalAreas.features[0].properties
+  let arr = agriculturalAreas.features.map((item) => {
+    return item.properties.Area;
+  });
+  
+  let geo = agriculturalAreas.features.map((item) => {
+    return item.geometry;
+  });
 
-    console.log(totalArea);
+  console.log(geo);
+  console.log(agriculturalAreas);
 
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const totalArea = arr.reduce(reducer) / 1000000; // square meters to square kilometers
+  console.log(totalArea.toFixed(2));
 
   return (
     <>
       <header className="heading">
         <h2>Agricultural Areas in Turkey</h2>
+        <br></br>
+        <p>
+          Total Agricultural area is {totalArea.toFixed(2)} square kilometers.
+        </p>
+        <br></br>
       </header>
 
-      <MapContainer center={[38, 32]} zoom={6} className="map-container">
-        {agriculturalAreas && (
-          <GeoJSON
-            attribution="Agricultural Areas in Turkey"
-            data={agriculturalAreas.features}
-          />
-        )}
-        {/*<TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />*/}
+      <MapContainer center={[38, 32]} zoom={6}>
+        <GeoJSON key={agriculturalAreas.features.id} data={agriculturalAreas} />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
       </MapContainer>
     </>
   );
