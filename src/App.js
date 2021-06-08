c0e1f0b39f9422f9da5e02f4382d6a7208c6def4
alitemel89 <alitemel89.ce@gmail.com>
2021-06-08 21:12:56 +0300

    update App.js and add geojson files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import urfaJson from "./data/urfa.json";
 
-import agriculturalAreas from "./data/agriculture_turkey.json";
+const agriculturalAreas = require("./data/agriculture_turkey.json");
 
 function App() {
   let arr = agriculturalAreas.features.map((item) => {
     return item.properties.Area;
   });
-  
+
+  let cityArea = urfaJson.features.map((item) => {
+    return item.properties.Area;
+  });
+
   let geo = agriculturalAreas.features.map((item) => {
     return item.geometry;
   });
 
-  console.log(geo);
   console.log(agriculturalAreas);
+  console.log(geo);
 
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
   const totalArea = arr.reduce(reducer) / 1000000; // square meters to square kilometers
+  console.log(totalArea.toFixed(2));
+
+  const cityAgriArea = cityArea.reduce(reducer) / 1000000; // square meters to square kilometers
   console.log(totalArea.toFixed(2));
 
   return (
@@ -29,11 +37,16 @@ function App() {
         <p>
           Total Agricultural area is {totalArea.toFixed(2)} square kilometers.
         </p>
+
+        <p>
+          Total Agricultural area of Urfa is {cityAgriArea.toFixed(2)} square kilometers.
+        </p>
         <br></br>
       </header>
 
       <MapContainer center={[38, 32]} zoom={6}>
-        <GeoJSON key={agriculturalAreas.features.id} data={agriculturalAreas} />
+        <GeoJSON data={urfaJson} />
+
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
